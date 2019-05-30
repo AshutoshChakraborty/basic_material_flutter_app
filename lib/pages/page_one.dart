eac493f1f6f081e24c56709f2dfd2aee5f729d3e
ashutosh <ashutoshmca13@gmail.com>
2019-05-31 02:26:54 +0530

    api added

--- lib/pages/page_one.dart
+++ lib/pages/page_one.dart
@@ -15,9 +15,10 @@ class PageOne extends StatelessWidget {
         child: FutureBuilder<CollectionResponse>(
           future: getCollectionResponse(),
           builder: (context, snapShot) {
-            if(snapShot.hasData) {
-              return Text(snapShot.data.shareUrl);
-            }else if (snapShot.hasError) {
+            if (snapShot.hasData) {
+              List<Collections> list = snapShot.data.collections;
+              return myListView(list);
+            } else if (snapShot.hasError) {
               return Text("${snapShot.data}");
             }
             return CircularProgressIndicator();
@@ -26,11 +27,26 @@ class PageOne extends StatelessWidget {
       ),
     );
   }
+
+  Widget myListView(List<Collections> list) {
+    return ListView.builder(
+      itemCount: list.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text(list[index].collection.title),
+          subtitle: Text(list[index].collection.description),
+          leading: Container(
+            height: 160,
+            child: Image.network(list[index].collection.imageUrl,height: 0,width: 160.0,fit: BoxFit.cover,))
+        );
+      },
+    );
+  }
 }
 
 Future<CollectionResponse> getCollectionResponse() async {
   String url =
-      "https://developers.zomato.com/api/v2.1/collections?city_id=2&count=3";
+      "https://developers.zomato.com/api/v2.1/collections?city_id=2&count=30";
   final response = await http
       .post(url, headers: {"user-key": "8a13280873a0f21dffaee2b028dee19a"});
   if (response.statusCode == 200) {
